0bf2a1b50babcb52452af5a7f87d78c3f2e020a2
Sreehari2004 <sreeharibaburaj2004@gmail.com>
2025-05-02 01:52:04 +0530

    allow server to connect to be visible across network

--- app.py
+++ app.py
@@ -184,4 +184,4 @@ def submit_contact():
         return jsonify({'error': 'Server error processing your request'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0',debug=True)
